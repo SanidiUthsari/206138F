ec978a1b57b5458c9f90023ee2a3c6ca00f11579
SanidiUthsari <sanidiuthsari@gmail.com>
2023-10-11 19:29:16 +0530

    Update the code

--- 206138F.py
+++ 206138F.py
@@ -1,6 +1,24 @@
-age = int(input("Enter your age:"))
-if age >= 18:
-    print("You can vote")
-else:
-    print("You cannot vote")
-    
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=================== RESTART: C:/Users/User/Desktop/206138F.py ==================
+Enter your age:7
+Traceback (most recent call last):
+  File "C:/Users/User/Desktop/206138F.py", line 2, in <module>
+    if age >= 18:
+TypeError: '>=' not supported between instances of 'str' and 'int'
+>>> 
+=================== RESTART: C:/Users/User/Desktop/206138F.py ==================
+Enter your age:8
+You cannot vote
+>>> 9
+9
+>>> 
+=================== RESTART: C:/Users/User/Desktop/206138F.py ==================
+Enter your age:88
+You can vote
+>>> 
+=================== RESTART: C:/Users/User/Desktop/206138F.py ==================
+Enter your age:8
+You cannot vote, come back when you are 18 :)
+>>> 
