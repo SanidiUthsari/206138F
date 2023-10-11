e275cf70995a13067eda34550d0e3fd093801eaa
SanidiUthsari <sanidiuthsari@gmail.com>
2023-10-11 20:49:53 +0530

    improve the code

--- 206138F.py
+++ 206138F.py
@@ -1,3 +1,6 @@
+print("welcome to the voting system :)")
+name = input("Enter your name:")
+print("Hello",name)
 minimum_Voting_age = 18 
 age = int(input("Enter your age:"))
 
@@ -6,10 +9,9 @@ if age >= minimum_Voting_age :
     vote = input("do you prefer to vote?").upper()
     if vote == "YES":
         print("Great! you can elect.")
-    elif vote == "NO"
+    elif vote == "NO":
         print(" may be next time :)")
     else:
         print("invalid input :(")
 else:
     print("You cannot vote, come back when you are 18 :)")
-    
