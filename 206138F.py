01ebf0f8be468ddb600c3d67029485e1892dfb01
SanidiUthsari <sanidiuthsari@gmail.com>
2023-10-11 20:07:06 +0530

    update by branch2

--- 206138F.py
+++ 206138F.py
@@ -1,24 +1,15 @@
-Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:58:18) [MSC v.1900 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-=================== RESTART: C:/Users/User/Desktop/206138F.py ==================
-Enter your age:7
-Traceback (most recent call last):
-  File "C:/Users/User/Desktop/206138F.py", line 2, in <module>
-    if age >= 18:
-TypeError: '>=' not supported between instances of 'str' and 'int'
->>> 
-=================== RESTART: C:/Users/User/Desktop/206138F.py ==================
-Enter your age:8
-You cannot vote
->>> 9
-9
->>> 
-=================== RESTART: C:/Users/User/Desktop/206138F.py ==================
-Enter your age:88
-You can vote
->>> 
-=================== RESTART: C:/Users/User/Desktop/206138F.py ==================
-Enter your age:8
-You cannot vote, come back when you are 18 :)
->>> 
+minimum_Voting_age = 18 
+age = int(input("Enter your age:"))
+
+if age >= minimum_Voting_age :
+    print("You can vote")
+    vote = input("do you prefer to vote?").upper()
+    if vote == "YES":
+        print("Great! you can elect.")
+    elif vote == "NO"
+        print(" may be next time :)")
+    else:
+        print("invalid input :(")
+else:
+    print("You cannot vote, come back when you are 18 :)")
+    
